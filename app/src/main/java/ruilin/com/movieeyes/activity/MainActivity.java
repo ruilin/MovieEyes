b535cec075408a3467e77a88790a5c321fee2db1
ruilin <zruilin@126.com>
2016-09-28 22:56:09 +0800

    界面优化

--- app/src/main/java/ruilin/com/movieeyes/activity/MainActivity.java
+++ app/src/main/java/ruilin/com/movieeyes/activity/MainActivity.java
@@ -205,17 +205,13 @@ public class MainActivity extends BaseActivity implements OnClickListener, Navig
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_favorite) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_record) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
